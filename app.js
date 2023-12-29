822858a9b78f696b3886500f378ab6e0bd97acdc
Himel Islam <himel7100@gmail.com>
2023-12-29 21:45:31 +0600

    paddles varialbes

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ const paddleSpeed = 50;
 
 let intervalID;
 let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballyDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+
+let paddle1 = {
+    width : 25,
+    height : 100,
+    x : 0,
+    y : 0
+}
+
+let paddle2 = {
+    width : 25,
+    height : 100,
+    x : gameWidth - 25,
+    y : gameWidth - 100
+}
+
