1e9916ceeedcfb50fe007d42cb38f7c7a00bba35
Himel Islam <himel7100@gmail.com>
2024-01-02 12:18:36 +0600

    score update

--- app.js
+++ app.js
@@ -141,6 +141,15 @@ function checkCollision() {
 
     if(ballX <= (paddle1.x + paddle1.width + ballRadius)){
         if(ballY > paddle1.y && ballY < paddle1.y + paddle1.height){
+            ballX = (paddle1.x + paddle1.width) + ballRadius; // if ball gets stuck
+            ballXDirection *= -1;
+            ballSpeed += 1;
+        }
+    }
+
+    if(ballX >= (paddle2.x - ballRadius)){
+        if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
+            ballX = paddle2.x - ballRadius;
             ballXDirection *= -1;
             ballSpeed += 1;
         }
@@ -181,7 +190,9 @@ function changeDirection(event) {
     }
 
 };
-function updateScore() { };
+function updateScore() {
+    scoreText.textContent = `${player1Score} : ${player2Score}`
+};
 
 
 function resetGame() { };
